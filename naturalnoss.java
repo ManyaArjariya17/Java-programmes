da0471d3a7218cc45c433d5662ee762935aa7289
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-08-05 23:36:14 +0530

    Add files via upload

---
+++ naturalnoss.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class naturalnoss{
+
+    public static void main(String[] args)
+    {
+        int i = 1;
+        do{
+            Scanner Sc = new Scanner(System.in);
+            int a = Sc.nextInt();
+            System.out.print("enter value of a");
+        }
+        while(i<=a)
+        {
+			System.out.println(i);
+			i++;
+		}
+    }
+    
+}
+
+// Name - Manya Arjariya
+// Roll-NO. - 36
+// Section - G
