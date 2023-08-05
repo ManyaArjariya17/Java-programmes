2d3c7c3ed46c2a322f3c8251a8e1e5fa654965cf
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-08-05 23:35:47 +0530

    Add files via upload

---
+++ naturalno.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class naturalno {
+
+	public static void main(String[] args)
+	{
+		int i =1;
+		Scanner Sc = new Scanner(System.in);
+		System.out.print("Enter the value a : ");
+		int a = Sc.nextInt();
+        System.out.println(" Natural Numbers are : ");
+
+		while(i<=a)
+
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
+
+// Name - Manya Arjariya
+// Roll-NO. - 36
+// Section - G
