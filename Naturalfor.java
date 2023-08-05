7cf2deb24df807fe2b96efed2c60832a3487da41
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-08-05 23:38:14 +0530

    Add files via upload

---
+++ Naturalfor.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Naturalfor
+{
+	public static void main(String[] args)
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter the number : ");
+		int n = sc.nextInt();
+        System.out.println("Numbers are : " );
+		for(int i=1; i<=n; i++) 
+		{
+			System.out.println(i);
+		}
+	}
+}
+
+// Name - Manya Arjariya
+// Roll-NO. - 36
+// Section - G
