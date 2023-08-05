3c1ac1b14c664b5cfe7ee23e14d34398c14cf8d0
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-08-05 23:36:38 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,16 @@
+import java.util.*;  
+public class Table {
+	public static void main(String[] args)
+	{
+	    Scanner Sc = new Scanner(System.in);
+        System.out.print("Enter the value a : ");
+		int a = Sc.nextInt();
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(a + " * " + i + " = "+ a * i);
+		}
+	}
+}
+
+// Name - Manya Arjariya
+// Roll-NO. - 36
+// Section  - G
